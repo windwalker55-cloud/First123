ef8b21b35b6c2089f42242244fe29da0750d51c2
Windwalker <diamondwindwalker@gmail.com>
2026-01-15 23:52:49 +0100

    typed some questions and get element by the id from thirdpage.html

---
+++ index.js
@@ -0,0 +1,14 @@
+import { sta223_2020,} from "./questions_and_answers/questions.js";
+const timer=document.getElementById("timer")
+const scoreDisplay=document.getElementById("score-display")
+const question=document.getElementById("question")
+const firstOption=document.getElementById("Firstoption")
+const secondOption=document.getElementById("secondoption")
+const thirdOption=document.getElementById("thirdoption")
+const fourthOption=document.getElementById("fourthoption")
+const previous=document.getElementById("previous")
+const submit=document.getElementById("submit")
+const next=document.getElementById("next")
+
+
+
---
+++ questions_and_answers/questions.js
@@ -0,0 +1,318 @@
+const sta223_2020=
+[
+    // first question
+{
+   question:"What is the primary purpose of sampling in statistics?",
+   A:"To study the entire universe directly",
+   B:"To avoid collecting any data" ,
+   C: "To learn about the population from a sample drawn from it",
+   D: "To eliminate variation completely "
+},
+
+// second question
+{
+   question:"How many elements are involved in the process of sampling?",
+   A:"Two",
+   B:"Three" ,
+   C:"Four",
+   D:"Five"
+},
+
+// third question
+{
+   question:"Which of the following is NOT one of the three elements of sampling mentioned?",
+   A:"Selecting the sample",
+   B:"Making inferences about the population" ,
+   C:"Conducting interviews",
+   D:"Collecting the information"
+},
+
+// fourth question
+{
+   question:"The 'Law of Statistical Regularity' is derived from",
+   A:"Laws of averages",
+   B:"MAthematical theory of probability" ,
+   C:"Central Limit Theory",
+   D:"Observational research"
+},
+
+//fifth question
+{
+   question:" According to the Law of Statistical Regularity, what must be true about the sample?",
+   A:" It must be very small",
+   B:"it must be biased" ,
+   C:"It must be chosen at random",
+   D:"It must include all population elements "
+}
+]
+
+
+
+
+const sta223_2021=
+[
+
+    // first question
+{
+   question:"What does the Law of Inertia of Large Numbers state?",
+   A:"Larger samples have more errors",
+   B:"Smaller samples are more accurate " ,
+   C: "Larger random samples tend to represent the population well",
+   D: "All samples must be of the same size"
+},
+
+// second question
+{
+   question:"What is one essential requirement of a good sample?",
+   A:" It should be small",
+   B:" It must avoid randomness" ,
+   C:"It must be representative of the universe",
+   D:"It must be collected manually"
+},
+
+// third question
+{
+   question:"Which method ensures representativeness in sampling?",
+   A:"Stratified method",
+   B:"Cluster sampling" ,
+   C:"judgment sampling",
+   D:"Random method"
+},
+
+// fourth question
+{
+   question:"In Systematic Sampling, if the calculated sampling interval is 5.75, what value should be used?",
+   A:"5",
+   B:"6" ,
+   C:"5.5",
+   D:"5.75"
+},
+
+//fifth question
+{
+   question:"What is the formula for the sampling interval (k) in Systematic Sampling?",
+   A:"k = Sample size ÷Population size",
+   B:"k = Sample size * Population size" ,
+   C:" k = Population size ÷ Sample size",
+   D:"k = Number of strata * Sample size"
+}
+
+]
+
+
+
+
+const sta223_2022=
+[
+
+    // first question
+{
+   question:"In Systematic Sampling, if N = 96 and n = 10, what is the value of k after approximation?",
+   A:"9",
+   B:"10" ,
+   C: "9.6",
+   D: "11"
+},
+
+// second question
+{
+   question:"A sufficient statistic is one that:",
+   A:"Contains all information about the parameter",
+   B:"Uses all data points",
+   C:"Is always unbiased",
+   D:"Uses all data points"
+},
+
+// third question
+{
+   question:"If p̂ = 0.5 and n = 400, the standard error is approximately",
+   A:"0.010",
+   B:"0.025",
+   C:"0.050",
+   D:"0.100"
+},
+
+// fourth question
+{
+   question:"When n < 30 and σ is unknown, we use",
+   A:"The normal distribution(Z)",
+   B:"The T distribution" ,
+   C:"The binomial distribution",
+   D:"Nonparametric method"
+},
+
+//fifth question
+{
+   question:"Type II error is committed when we",
+   A:"Reject H₀ when it's true",
+   B:"Fail to reject H₀ when H₀ is false" ,
+   C:"Accept H₁ when it's true ",
+   D:"Calculate the wrong p-value"
+}
+
+]
+
+
+
+
+
+
+
+const sta223_2023=
+[
+
+    // first question
+{
+   question:"The set notation that adequately express the statement below given the events A,B,C, All the events occur",
+   A:" A U B U C ",
+   B:": A U C U D " ,
+   C: ": A U B U D",
+   D: " (A U B U C)1"
+},
+
+// second question
+{
+   question:"Evaluate 5P3/6P2 ",
+   A:"40/3",
+   B:"41/3",
+   C:"42/3",
+   D:"43/3"
+},
+
+// third question
+{
+   question:"A coin is tossed ten times, how many possible outcome do we have",
+   A:"1000",
+   B:"1023" ,
+   C:"1024.2",
+   D:"1024"
+},
+
+// fourth question
+{
+   question:"If two events A and B are said to be mutually exclusive, then",
+   A:"P(AUB) = P(A) + P(C) P(AnB)",
+   B:"P(AUB) = P(A) + P(B) P(AnB)" ,
+   C:" P(AUB) = P(B) + P(C)",
+   D:" P(AUB) = P(A) + P(B)"
+},
+
+//fifth question
+{
+   question:"In how many ways can 9 books be arranged on a shelf if any arrangement is possible?",
+   A:"34510",
+   B:"39087" ,
+   C:"38762",
+   D:"362880"
+}
+
+]
+
+
+
+
+const sta223_2024=
+[
+
+    // first question
+{
+   question:"Statistical events can be generated using one of the following",
+   A:"Set Theory ",
+   B:"calculus" ,
+   C: "Integration",
+   D: "All of the above"
+},
+
+// second question
+{
+   question:"In an agricultural experiment, different fertilizers are considered as",
+   A:"Experimental units",
+   B:"Factors " ,
+   C:"Levels",
+   D:"Treatments"
+},
+
+// third question
+{
+   question:"In a Latin Square Design (LSD), how many factors are used?",
+   A:"One",
+   B:"Two" ,
+   C:"Three",
+   D:"Four"
+},
+
+// fourth question
+{
+   question:"In an LSD with 4 treatments, how many experimental units are required?",
+   A:"8",
+   B:"22" ,
+   C:"12",
+   D:"16"
+},
+
+//fifth question
+{
+   question:"In a CRD, any difference observed among the experimental units receiving different treatments is considered as",
+   A:"A planned effect",
+   B:"A systematic variation" ,
+   C:"Experiemental error",
+   D:"A treatment effect "
+}
+
+]
+
+
+
+
+const sta223_2025=
+[
+
+    // first question
+{
+   question:"In GLSD, the model is complete addictive",
+   A:"True",
+   B:"False" ,
+   C: "Not sure",
+   D: "None of the above"
+},
+
+// second question
+{
+   question:"In a Graeco-Latin Square Design, how many factors are controlled?",
+   A:"One",
+   B:"Two",
+   C:"Three",
+   D:"Four"
+},
+
+// third question
+{
+   question:"In the RCBD model, which term represents random variation? ",
+   A:"μ",
+   B:"εij" ,
+   C:"βj",
+   D:"τi"
+},
+
+// fourth question
+{
+   question:"What is the main purpose of replication in an experiment?",
+   A:"To increase bias in results ",
+   B:"To reduce the number of treatments" ,
+   C:"To estimate experimental error",
+   D:"To change the treatment level "
+},
+
+//fifth question
+{
+   question:"PROFESSOR BOAZ WONDER and STATISTICIAN COMIBERRY conducted an experiment to evaluate the effectiveness of four different teaching methods on student performance. Using a Completely Randomized Design (CRD), they both obtained an F-calculated value of 13.4, with 3 and 21 degrees of freedom respectively. At a 5% level of significance, what decision should they make based on the ANOVA results?",
+   A:"Reject the null hypothesis",
+   B:"Fail to reject the null hyphotesis" ,
+   C:"The sample size is insufficient for a reliable test",
+   D:"The F-distribution is inappropriate for this analysis "
+}
+
+]
+
+
+export{sta223_2020,sta223_2021, sta223_2022, sta223_2023, sta223_2024, sta223_2025}
